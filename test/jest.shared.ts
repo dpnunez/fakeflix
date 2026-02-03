1b37f74464ffa52cd3c72d9b1b1a6c8d15f466fd
Daniel Núñez <daniel.portonunez@gmail.com>
2026-02-03 20:54:44 -0300

    feat: update TypeScript paths and enhance Jest configuration
    - Add new module paths for identity and shared libraries in tsconfig.json to improve module resolution.
    - Modify Jest configuration to allow for nested test files in e2e testing, enhancing test discovery.

--- test/jest.shared.ts
+++ test/jest.shared.ts
@@ -4,7 +4,9 @@ export default {
   testEnvironment: 'node',
   moduleNameMapper: {
     "^@contentModule/(.*)$": "<rootDir>/src/module/content/$1",
+    "^@indentityModule/(.*)$": "<rootDir>/src/module/identity/$1",
     "^@sharedModule/(.*)$": "<rootDir>/src/module/shared/module/$1",
+    "^@sharedLibs/(.*)$": "<rootDir>/src/module/shared/$1",
     '^@src/(.*)$': '<rootDir>/src/$1',
     '^@database/(.*)$': '<rootDir>/database/$1',
     '^@testInfra/(.*)$': '<rootDir>/test/$1',
